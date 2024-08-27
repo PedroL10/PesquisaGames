391c76f22eaf37ecbd9e698a3336db1c862ff5bb
Pedro Goncalves <pepluisgon01@gmail.com>
2024-08-27 16:30:39 -0300

    POST records

---
+++ backend/src/main/java/com/pedrolg/games/services/RecordService.java
@@ -0,0 +1,40 @@
+package com.pedrolg.games.services;
+
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.pedrolg.games.dto.RecordDTO;
+import com.pedrolg.games.dto.RecordInsertDTO;
+import com.pedrolg.games.entities.Game;
+import com.pedrolg.games.entities.Record;
+import com.pedrolg.games.repositories.GameRepository;
+import com.pedrolg.games.repositories.RecordRepository;
+
+@Service
+public class RecordService {
+	
+	@Autowired
+	private RecordRepository repository;
+	
+	@Autowired
+	private GameRepository gameRepository;
+	
+	@Transactional
+	public RecordDTO insert(RecordInsertDTO dto) {
+		Record entity = new Record();
+		entity.setName(dto.getName());
+		entity.setAge(dto.getAge());
+		entity.setMoment(Instant.now());
+		
+		Game game = gameRepository.getOne(dto.getGameId());
+		entity.setGame(game);
+		
+		entity = repository.save(entity);
+		
+		return new RecordDTO(entity);
+	}
+
+}
