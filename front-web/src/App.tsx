21b04242c0d0bb455ac2cc0c8438cb8dc45ddf3a
Pedro Goncalves <pedro.goncalves@pasi.com.br>
2024-08-30 16:51:40 -0300

    Pagina inicial

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-
 import './App.css';
+import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
-   <h1>DevSuperior</h1>
+    <div>
+      <Header/>
+      <Home/>
+     
+    </div>
   );
 }
 
